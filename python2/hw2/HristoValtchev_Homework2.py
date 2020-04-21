15ae82eb50a306d57a57a17f292d1123dcf8bf70
Hristo Valtchev <hristo@valtchev.com>
2020-04-21 07:00:16 -0700

    Completed Rectangle Added Debug Statements for Square and Rectangle

--- python2/hw2/HristoValtchev_Homework2.py
+++ python2/hw2/HristoValtchev_Homework2.py
@@ -65,11 +65,12 @@ class Rectangle():
     """Represents a rectangle
     """
 
-    def __init__(self, size, hChar, vChar, cornerChar):
-        ''' Initializes a square
+    def __init__(self, length, width, hChar, vChar, cornerChar):
+        ''' Initializes a rectangle
      in the size, the character to used for the horizontal edge, vertical edge, and corners.
         '''
-        self.size = size
+        self.length = length
+        self.width = width
         self.hChar = hChar
         self.vChar = vChar
         self.cornerChar = cornerChar
@@ -80,24 +81,34 @@ class Rectangle():
         '''
         print()
 
-        firstLast = str(self.cornerChar) + str(self.hChar) * (self.size - 2) + str(self.cornerChar)
-        middle = str(self.vChar) + (' ' * (self.size - 2)) + str(self.vChar)
+        firstLast = str(self.cornerChar) + str(self.hChar) * (self.length - 2) + str(self.cornerChar)
+        middle = str(self.vChar) + (' ' * (self.length - 2)) + str(self.vChar)
 
-        for x in range(0, self.size):
-            if x == 0 or x == self.size - 1:
+        for x in range(0, self.width):
+            if x == 0 or x == self.width - 1:
                 print(firstLast)
             else:
                 print(middle)
 
-    def getSize(self):
+    def getLength(self):
         ''' Returns the size of an edge of the Square '''
-        return self.size
+        return self.length
 
-    def setSize(self, size):
-        self.size = size
+    def setLength(self, length):
+        self.length = length
+
+    def getWidth(self):
+        ''' Returns the size of an edge of the Square '''
+        return self.width
+
+    def setWidth(self, width):
+        self.length = width
 
     def getArea(self):
-        return self.size * self.size
+        return self.length * self.width
+
+    def getPerimeter(self):
+        return 2 * (self.length + self.width)
 
     def setHorizontalChar(self, newHChar):
         ''' Set a new horizontal character '''
@@ -116,12 +127,12 @@ class Rectangle():
 # Create a square of size 5
 oSquare1 = Square(5, '-', '|', '*')  # in size, horizonal, vertical, and edge characters
 oSquare1.show()
-print('Size is:', oSquare1.getSize(), " area is:", oSquare1.getArea())
+print('Size is:', oSquare1.getSize(), " Area is:", oSquare1.getArea())
 
 # Create another square of size 10
 oSquare2 = Square(10, '-', '|', '*')
 oSquare2.show()
-print('Size is:', oSquare2.getSize(), " area is:", oSquare2.getArea())
+print('Size is:', oSquare2.getSize(), " Area is:", oSquare2.getArea())
 
 # Tell the first square to modify its data
 oSquare1.setSize(7)
@@ -129,7 +140,7 @@ oSquare1.setHorizontalChar('^')
 oSquare1.setVerticalChar('?')
 oSquare1.setCornerChar('$')
 oSquare1.show()
-print('Size is:', oSquare1.getSize(), " area is:", oSquare1.getArea())
+print('Size is:', oSquare1.getSize(), " Area is:", oSquare1.getArea())
 print()
 
 # Add code here to ask the user questions, and create and show a new Square based on the answers
@@ -140,5 +151,17 @@ cChar = input('What character  to use for the corners: ')
 
 oSquare3 = Square(size, hChar, vChar, cChar)
 oSquare3.show()
-print('Size is:', oSquare3.getSize(), " area is:", oSquare3.getArea())
+print('Size is:', oSquare3.getSize(), " Area is:", oSquare3.getArea())
+print()
+
+# Test code for Rectangle
+length = int(input('What is the (integer) length of the Rectangle: '))
+width = int(input('What is the (integer) width of the Rectangle: '))
+hChar = input('What character to use for the horizontal edges: ')
+vChar = input('What character to use for the vertical edges: ')
+cChar = input('What character to use for the corners: ')
+
+oRectangle1 = Rectangle(length, width, hChar, vChar, cChar)
+oRectangle1.show()
+print('Perimeter is:', oRectangle1.getPerimeter(), " Area is:", oRectangle1.getArea())
 print()
