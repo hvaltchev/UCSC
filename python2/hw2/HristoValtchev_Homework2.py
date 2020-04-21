2c32bb01df2be2d16abf06099fda6f633e9422e3
Hristo Valtchev <hristo@valtchev.com>
2020-04-21 05:31:32 -0700

    Class Functions - Done

--- python2/hw2/HristoValtchev_Homework2.py
+++ python2/hw2/HristoValtchev_Homework2.py
@@ -16,7 +16,7 @@ class Square():
 
     def __init__(self, size, hChar, vChar, cornerChar):
         ''' Initializes a square
-        Pass in the size, the character to used for the horizontal edge, vertical edge, and corners.
+     in the size, the character to used for the horizontal edge, vertical edge, and corners.
         '''
         self.size = size
         self.hChar = hChar
@@ -30,44 +30,58 @@ class Square():
         print()
         print('This line should be replaced with lines to draw this Square.')
 
+        for x in range(0, self.size - 1):
+            for y in range(0, self.size - 1):
+                if (x or y) == 0:
+                    print(self.cornerChar)
+                if (x or y) == self.size - 1:
+                    print(self.cornerChar)
+                else:
+                    print(self.hChar)
+
     def getSize(self):
         ''' Returns the size of an edge of the Square '''
-        pass
+        return self.size
+
+    def setSize(self, size):
+        self.size = size
+
+    def getArea(self):
+        return self.size * self.size
 
     def setHorizontalChar(self, newHChar):
         ''' Set a new horizontal character '''
-        pass
+        self.hChar = newHChar
+
         
     def setVerticalChar(self, newVChar):
         ''' Set a new vertical character '''
-        pass
+        self.vChar = newVChar
+
 
     def setCornerChar(self, newCornerChar):
         ''' Set a new corner character '''
-        pass
-
-    # Must add 2 additional methods: setSize and getArea
-
+        self.cornerChar = newCornerChar
 
 # Test code
 # Create a square of size 5
-oSquare1 = Square(5, '-', '|', '*')  # pass in size, horizonal, vertical, and edge characters
+oSquare1 = Square(5, '-', '|', '*')  # in size, horizonal, vertical, and edge characters
 oSquare1.show()
 print('Size is:', oSquare1.getSize(), " area is:", oSquare1.getArea())
 
-# Create another square of size 10
-oSquare2 = Square(10, '-', '|', '*')
-oSquare2.show()
-print('Size is:', oSquare2.getSize(), " area is:", oSquare2.getArea())
+# # Create another square of size 10
+# oSquare2 = Square(10, '-', '|', '*')
+# oSquare2.show()
+# print('Size is:', oSquare2.getSize(), " area is:", oSquare2.getArea())
 
-# Tell the first square to modify its data
-oSquare1.setSize(7)
-oSquare1.setHorizontalChar('^')
-oSquare1.setVerticalChar('?')
-oSquare1.setCornerChar('$')
-oSquare1.show()
-print('Size is:', oSquare1.getSize(), " area is:", oSquare1.getArea())
-print()
+# # Tell the first square to modify its data
+# oSquare1.setSize(7)
+# oSquare1.setHorizontalChar('^')
+# oSquare1.setVerticalChar('?')
+# oSquare1.setCornerChar('$')
+# oSquare1.show()
+# print('Size is:', oSquare1.getSize(), " area is:", oSquare1.getArea())
+# print()
 
 
 # Add code here to ask the user questions, and create and show a new Square based on the answers
