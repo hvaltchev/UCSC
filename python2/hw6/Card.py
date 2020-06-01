ad99e41374403ecde2fdc40633f432563849d2ea
hvaltchev <hristo@valtchev.com>
2020-06-01 08:47:38 -0700

    Done

--- python2/hw6/Card.py
+++ python2/hw6/Card.py
@@ -12,36 +12,26 @@ class Card():
         self.suit = suit
         self.value = value
 
-        # create two image objects
-        self.cardImage = pygwidgets.Image(window, (100, 200), "images/BackOfCard.png")
-        self.backImage = pygwidgets.Image(window, (100, 200), "images/BackOfCard.png")
+        # create an image object
+        self.cardImage = pygwidgets.Image(window, (100, 200), 'images/BackOfCard.png')
 
     def conceal(self):
-        print('Must conceal the card here')
-        self.backImage.draw()
+        self.cardImage.replace('images/BackofCard.png')
 
     def setLoc(self, locTuple):
-        print('Called setLoc method, passed in', locTuple)
-        pass
+        self.cardImage.setLoc(locTuple)
 
     def reveal(self):
-        print('Must reveal card here')
-        self.cardImage.draw()
+        self.cardImage.replace('images/' + self.getName() + '.png')
 
     def getName(self):
-        print('Get the name of the card here')
         return self.rank + ' of ' + self.suit
 
     def getValue(self):
-        print('Get the value of the card here')
         return self.value
 
     def draw(self):
-        print('Draw the card here')
         self.cardImage.draw()
 
     def getCardNameAndValue(self):
-        print("Get the card name and value here")
-        name = self.getName()
-        value = self.value()
-        return name, value
+        return self.getName(), self.value()
